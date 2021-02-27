2341de9722f775de10a7537d68ff8e839470cd75
gcdhg <gcdhg@outlook.com>
2021-02-27 18:14:45 +0300

    random field test added

---
+++ src/__tests__/fields/randomField.test.ts
@@ -0,0 +1,52 @@
+import fetch from "node-fetch";
+import database from "../../db/config/database";
+
+describe("article fields", () => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+      Origin: "http://localhost:8080",
+    },
+  };
+
+  const variables = {
+    title: "hello there",
+    body: "general kenobi",
+  };
+
+  it("get random field", async () => {
+    let art: any = await database.models.Article.findOne();
+    art = art.toJSON();
+
+    let query = `
+      query {
+        arcticle(id: ${art.id}) {
+          randomfield
+        }
+      }`;
+
+    let body = JSON.stringify({ query });
+
+    const res = await fetch("http://localhost:8080/graphql", {
+      ...options,
+      body,
+    });
+
+    let articles: any = await database.models.Article.findAll();
+    articles = articles.map((a) => a.toJSON());
+
+    expect(res.status).toBe(200);
+    let json: any = await res.json();
+    json = json.data.arcticle.randomfield;
+    expect(json[0]).not.toEqual(json[1]);
+
+    json.forEach((index) => {
+      const found = articles.filter((a) => a.id === index);
+      expect(found).toHaveLength(1);
+    });
+  });
+});
